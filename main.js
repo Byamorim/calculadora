b4e83838f53df53f97f7519ed176cf542df70ef1
Byamorim <105076357+Byamorim@users.noreply.github.com>
2024-02-27 10:44:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function add() {
+    var adicao =
+    Number(document.getElementById("n1").value) +
+    Number(document.getElementById("n2").value);
+    document.getElementById("resultado").innerHTML="adição "+adicao;
+    }
+    
